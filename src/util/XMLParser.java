199354819a1941bed97161cb01933fe69b0bde06
Ramil Shaymardanov <ramilmsh@r.local>
2017-09-17 18:16:10 -0400

    txt->md

--- src/util/XMLParser.java
+++ src/util/XMLParser.java
@@ -1,17 +1,23 @@
+package util;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
-
+import org.w3c.dom.Document;
 
 /**
  * Class that implements parsing XML
  */
 public class XMLParser {
 
-    public static Document parse() {
+    public static Document parse(String xml) {
+        return null;
+    }
+
+    public static Document parse(String xml, String xsd) {
 
     }
 
-    private static boolean validate(String xml, String xsd) {
+    private static void validate(Document xml) {
 
     }
 }
